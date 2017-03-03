a04867c29a344001a9226f50ac1633a3ba8dcc10
Scott Haile Robertson <shr11@duke.edu>
2017-03-03 16:39:19 -0500

    added a package directory for spectroscopy code

--- setup.m
+++ setup.m
@@ -3,4 +3,5 @@ rootDistribDir = pwd();
 
 % Add GE package to path
 disp('Adding spectroscopy package to MATLAB path...');
-path(genpath(rootDistribDir),path);
+
+path(genpath([rootDistribDir filesep() 'SpectroscopyPackage']) ,path);
